e92bbd36bf416f771fefb05537c08900d7753af5
DiogoMeneses-stack <diogomeneses0605xyz@gmail.com>
2022-06-26 22:03:04 -0300

    Move ball.

--- src/pong/Pong.java
+++ src/pong/Pong.java
@@ -45,12 +45,12 @@ public class Pong implements ActionListener, KeyListener {
 		jframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		jframe.add(renderer);
 		jframe.addKeyListener(this);
-		start();
 
 		timer.start();
 	}
 
 	public void start() {
+		gameStatus = 2;
 		player1 = new Paddle(this, 1);
 		player2 = new Paddle(this, 2);
 		ball = new Ball(this);
@@ -69,6 +69,7 @@ public class Pong implements ActionListener, KeyListener {
 		if (down) {
 			player2.move(false);
 		}
+		ball.update(player1, player2);
 
 	}
 
@@ -161,16 +162,15 @@ public class Pong implements ActionListener, KeyListener {
 		}
 		if (id == KeyEvent.VK_SHIFT && gameStatus == 0) {
 			bot = true;
-			gameStatus = 2;
+			start();
 		}
 
 		if (id == KeyEvent.VK_SPACE) {
 
 			if (gameStatus == 0) {
-				gameStatus = 2;
+				start();
 				bot = false;
-			}
-			else if (gameStatus == 1) {
+			} else if (gameStatus == 1) {
 				gameStatus = 2;
 			}
 
